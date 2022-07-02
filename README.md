eeaeca9884050121ac9791a2f4e3a0ca01de8843
yaoysyao <1303703312@qq.com>
2022-07-03 00:07:12 +0800

    Init commit code

---
+++ README.md
@@ -0,0 +1,63 @@
+# GLaDOS_Checkin
+
+GLaDOS automatic check-in bypassing CloudFlare using github action
+
+## 功能描述
+
+1. 每日自动进行签到（本项目可通过CloudFlare反爬机制）
+2. 支持多用户签到，多个Cookie之间采用`&&`手动分割
+3. 本项目可触发Github Notifications，出现Cookie过期/签到失败等可自动发送Email
+3. 本项目包含Github Actions keep alive模块，可自动激活Github Actions
+
+## 使用方法
+
+### 1. 添加 Cookie 至 Secrets
+
+- 登陆[GLaDOS](https://glados.rocks/)后，F12打开开发者工具。
+- 刷新网页，并在浏览器中提取复制`Cookie`项（本程序可处理`Cookie:`前缀，使用者复制该项时是否具有前缀均可)
+
+<p align="center">
+  <img src="imgs/Step1.png" />
+</p>
+
+- 在项目页面，依次点击`Settings`-->`Secrets`-->`Actions`-->`New repository secret`
+
+<p align="center">
+  <img src="imgs/Step2.png" />
+</p>
+
+- 建立名为`GLADOS_COOKIE`的 secret，值为复制的`Cookie`内容，最后点击`Add secret`
+- secret名字必须为`GLADOS_COOKIE`，大小写敏感
+- 支持多用户签到，多个Cookie之间采用`&&`手动分割完成后填入`GLADOS_COOKIE`即可
+- 为保护隐私，不在日志中输出任何Id信息，请自行分辨账号顺序
+
+<p align="center">
+  <img src="imgs/Step3.png" />
+</p>
+
+### 2. 添加 PUSHPLUS_TOKEN 至 Secrets,如果不想推送通知可以不填写此项
+- 建立名为`PUSHPLUS_TOKEN`的 secret，值为复制的`pushplus（推送加平台的token）`，最后点击`Add secret`
+- 登陆[pushplus](http://www.pushplus.plus/)
+
+<p align="center">
+  <img src="imgs/pushplus_token.png" />
+</p>
+
+- 推送消息略有延迟，但是影响不大，每个账号发送一条通知消息
+<p align="center">
+  <img src="imgs/pushplus_message.png" />
+</p>
+
+- 推送消息详情
+<p align="center">
+  <img src="imgs/pushplus_message_info.png" />
+</p>
+
+### 3. 启用 Actions
+- 在项目页面，依次点击`Actions`-->`glados`-->`Run workflow`-->`Run workflow`以激活Actions
+<p align="center">
+  <img src="imgs/Step4.png" />
+</p>
+
+- 本项目由Workflow控制，每日0时30分自动执行。
+- 本项目目前可以正常运行，如果有其他使用问题请在Issues留言。
---
+++ glados/glados.py
@@ -0,0 +1,140 @@
+# encoding=utf8
+import io
+import re
+import sys
+import time
+import json
+import subprocess
+import requests
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+import undetected_chromedriver as uc
+from selenium.webdriver.support.ui import WebDriverWait
+import os
+
+
+def get_driver_version():
+    cmd = r'''powershell -command "&{(Get-Item 'C:\Program Files\Google\Chrome\Application\chrome.exe').VersionInfo.ProductVersion}"'''
+    try:
+        out, err = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+        out = out.decode('utf-8').split(".")[0]
+        return out
+    except IndexError as e:
+        print('Check chrome version failed:{}'.format(e))
+        return 0
+
+
+def glados_checkin(driver):
+    checkin_url = "https://glados.rocks/api/user/checkin"
+    checkin_query = """
+        (function (){
+        var request = new XMLHttpRequest();
+        request.open("POST","%s",false);
+        request.setRequestHeader('content-type', 'application/json');
+        request.send('{"token": "glados.network"}');
+        return request;
+        })();
+        """ % (checkin_url)
+    checkin_query = checkin_query.replace("\n", "")
+    resp = driver.execute_script("return " + checkin_query)
+    resp = json.loads(resp["response"])
+    return resp["code"], resp["message"]
+
+
+def glados_status(driver):
+    status_url = "https://glados.rocks/api/user/status"
+    status_query = """
+        (function (){
+        var request = new XMLHttpRequest();
+        request.open("GET","%s",false);
+        request.send(null);
+        return request;
+        })();
+        """ % (status_url)
+    status_query = status_query.replace("\n", "")
+    resp = driver.execute_script("return " + status_query)
+    resp = json.loads(resp["response"])
+    return resp["code"], resp["data"]
+
+
+def pushplus_message(token, message):
+    print('start push message to pushplus:', str(message))
+    payload = {'token': token, "channel": "wechat", "template": "html", "content": message, "title": "签到状态"}
+    resp = requests.post("http://www.pushplus.plus/send", params=payload)
+    print('pushplus code:', resp.status_code)
+    resp.close()
+
+
+def glados(cookie_string, pushplus_token=None):
+    options = uc.ChromeOptions()
+    options.add_argument("--disable-popup-blocking")
+
+    version = get_driver_version()
+    driver = uc.Chrome(version_main=version, options=options)
+
+    # Load cookie
+    driver.get("https://glados.rocks")
+
+    if cookie_string.startswith("cookie:"):
+        cookie_string = cookie_string[len("cookie:"):]
+    cookie_dict = [
+        {"name": x[:x.find('=')].strip(), "value": x[x.find('=') + 1:].strip()}
+        for x in cookie_string.split(';')
+    ]
+
+    driver.delete_all_cookies()
+    for cookie in cookie_dict:
+        if cookie["name"] in ["koa:sess", "koa:sess.sig"]:
+            driver.add_cookie({
+                "domain": "glados.rocks",
+                "name": cookie["name"],
+                "value": cookie["value"],
+                "path": "/",
+            })
+
+    driver.get("https://glados.rocks")
+    WebDriverWait(driver, 240).until(
+        lambda x: x.title != "Just a moment..."
+    )
+
+    checkin_code, checkin_message = glados_checkin(driver)
+    if checkin_code == -2: checkin_message = "Login fails, please check your cookie."
+    print(f"【Checkin】{checkin_message}")
+
+    if checkin_code != -2:
+        status_code, status_data = glados_status(driver)
+        left_days = int(float(status_data["leftDays"]))
+        print(f"【Status】Left days:{left_days}")
+        try:
+            if pushplus_token is not None and len(pushplus_token) > 0:
+                pushplus_message(pushplus_token, '签到时间:' + str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + ',message:【Checkin】' + checkin_message + ',【Status】Left days:' + str(left_days) + ',【email】' + status_data["email"])
+            else:
+                print('The pushplus_token is none')
+        except Exception:
+            print('push message error')
+
+    driver.close()
+    driver.quit()
+
+    return checkin_code
+
+
+pushplus_token = None
+
+if __name__ == "__main__":
+    cookie_string = sys.argv[1]
+    pushplus_token = os.environ['PUSHPLUS_TOKEN']
+
+    assert cookie_string
+
+    cookie_string = cookie_string.split("&&")
+    checkin_codes = list()
+
+    for idx, cookie in enumerate(cookie_string):
+        print(f"【Account_{idx + 1}】:")
+        checkin_code = glados(cookie, pushplus_token=pushplus_token)
+        checkin_codes.append(checkin_code)
+
+    assert -2 not in checkin_codes, "At least one account login fails."
+    assert checkin_codes.count(0) + checkin_codes.count(1) == len(checkin_codes), "Not all the accounts check in successfully."
